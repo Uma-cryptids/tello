5486e1d156598cdb4f2e5ccbd748b3c5858b211c
tetra <three.tetration.three@gmail.com>
2021-10-26 13:46:25 +0900

    YESSSSS

--- operation/gui.py
+++ operation/gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from djitellopy import tello
 import math
 import time
 
@@ -7,38 +8,53 @@ class Drone:
     MAX = 50
     TAU = -0.05
 
-    def __init__(self):
+    def __init__(self, drone):
         self.speed = [0, 0, 0, 0]
         self.cnt = 0
         self.last = ""
         self.lasttime = time.time()
         self.flag = True
+        self.liftoff = False
+        self.drone = drone
+        self.drone.connect()
 
     def enter(self, key: str):
-        if key != self.last:
-            self.cnt = 0
 
-        if time.time() - self.lasttime > 0.2:
-            self.cnt = 0
+        if key == "q":
+            if not self.liftoff:
+                self.drone.takeoff()
+                self.liftoff = True
+            else:
+                self.drone.land()
+                self.liftoff = False
 
-        self.flag = True
+        if self.liftoff:
+            if key != self.last:
+                self.cnt = 0
 
-        if key == "w":
-            self.speed[0] = int(self.MAX*(1 - math.exp(self.TAU*self.cnt)))
-        elif key == "s":
-            self.speed[0] = int(self.MAX*(math.exp(self.TAU*self.cnt) - 1))
-        elif key == "a":
-            self.speed[1] = int(self.MAX*(math.exp(self.TAU*self.cnt) - 1))
-        elif key == "d":
-            self.speed[1] = int(self.MAX*(1 - math.exp(self.TAU*self.cnt)))
-        elif key == "space":
-            self.speed[2] = int(self.MAX*(1 - math.exp(self.TAU*self.cnt)))
-        elif key == "x":
-            self.speed[2] = int(self.MAX*(math.exp(self.TAU*self.cnt) - 1))
-        elif key == "Right":
-            self.speed[3] = int(self.MAX*(1 - math.exp(self.TAU*self.cnt)))
-        elif key == "Left":
-            self.speed[3] = int(self.MAX*(math.exp(self.TAU*self.cnt) - 1))
+            if time.time() - self.lasttime > 0.2:
+                self.cnt = 0
+
+            self.flag = True
+
+            if key == "w":
+                self.speed[0] = int(self.MAX*(1 - math.exp(self.TAU*self.cnt)))
+            elif key == "s":
+                self.speed[0] = int(self.MAX*(math.exp(self.TAU*self.cnt) - 1))
+            elif key == "a":
+                self.speed[1] = int(self.MAX*(math.exp(self.TAU*self.cnt) - 1))
+            elif key == "d":
+                self.speed[1] = int(self.MAX*(1 - math.exp(self.TAU*self.cnt)))
+            elif key == "space":
+                self.speed[2] = int(self.MAX*(1 - math.exp(self.TAU*self.cnt)))
+            elif key == "x":
+                self.speed[2] = int(self.MAX*(math.exp(self.TAU*self.cnt) - 1))
+            elif key == "Right":
+                self.speed[3] = int(self.MAX*(1 - math.exp(self.TAU*self.cnt)))
+            elif key == "Left":
+                self.speed[3] = int(self.MAX*(math.exp(self.TAU*self.cnt) - 1))
+
+            self.drone.send_rc_control(*self.speed)
 
         self.last = key
         self.lasttime = time.time()
@@ -55,10 +71,15 @@ class Operator(tk.Frame):
     def __init__(self, parent, drone):
         tk.Frame.__init__(self, parent, width=500, height=400)
 
+        self.parent = parent
         self.tello = drone
 
-        self.label = tk.Label(self, text="", width=50)
-        self.label.pack(fill="both", padx=100, pady=100)
+        self.takeofflabel = tk.Label(self, text="landing", width=50)
+        self.takeofflabel.pack(fill="both", padx=10, pady=10)
+
+        self.label = tk.Label(self, text="speed: " +
+                              str(self.tello.speed), width=50)
+        self.label.pack(fill="both", padx=10, pady=10)
 
         self.label.bind("<KeyPress>", self.press)
         self.label.bind("<KeyRelease>", self.release)
@@ -68,6 +89,10 @@ class Operator(tk.Frame):
 
     def press(self, event):
         self.tello.enter(event.keysym)
+        if self.tello.liftoff:
+            self.takeofflabel.configure(text="takeoff")
+        else:
+            self.takeofflabel.configure(text="landing")
         self.label.configure(text="speed: "+str(self.tello.speed))
 
     def release(self, _event):
@@ -77,6 +102,6 @@ class Operator(tk.Frame):
 
 if __name__ == "__main__":
     root = tk.Tk()
-    mytello = Drone()
+    mytello = Drone(tello.Tello())
     Operator(root, mytello).pack(fill="both", expand=True)
     root.mainloop()
